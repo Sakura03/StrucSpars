fe22cd72eda7aea226007a2b40187f23e06d4c0d
Xinyu Zhang <xyzhang_nku@qq.com>
2021-06-17 14:37:35 +0800

    Remove .sh files

--- run_cifar.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES='0' python3 train_cifar.py -a "resnet20" --data "./data" --dataset "cifar10" --save-path "results/cifar10-resnet20-prune-percent-0.4" --prune-percent "0.4"
--- run_imagenet.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES='0,1,2,3' python3 -m torch.distributed.launch --nproc_per_node=4 --master_port=2345 train_imagenet.py \
-                -a "resnet50" \
-                -j "4" \
-                --data "/mnt/truenas/scratch/hzh/datasets/imagenet_rec" \
-                --save-path "results/imagenet-resnet50-prune-percent-0.35" \
-                --batch-size "64" \
-                --prune-percent "0.35"
